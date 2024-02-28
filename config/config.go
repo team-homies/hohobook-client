9f3c8b2a560f0efc14b1c7e7e5573cd259878fc9
WGCAT <seed9878@naver.com>
2024-02-28 21:43:44 +0900

    Feat : config 수정

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
-import "sync"
+import (
+	"sync"
+)
 
 type CallInfo struct {
 	HOST string
@@ -9,18 +11,39 @@ type CallInfo struct {
 }
 
 type path struct {
-	GET  get
-	POST post
+	GET    get
+	POST   post
+	PUT    put
+	DELETE delete
 }
 
 type get struct {
-	METHOD     string
-	BookSearch string
+	METHOD             string
+	BookList           string
+	BookTitleSearch    string
+	BookDetailSearch   string
+	RentUserInfoSearch string
+	BookSearch_Update  string
 }
 
 type post struct {
+	METHOD             string
+	BookRegist         string
+	BookRegistDetail   string
+	RentUserInfoRegist string
+}
+
+type put struct {
 	METHOD     string
-	BookRegist string
+	BookUpdate string
+	BookRent   string
+	BookReturn string
+}
+
+type delete struct {
+	METHOD               string
+	BookRemove           string
+	ReturnUserInfoRemove string
 }
 
 var (
@@ -35,12 +58,47 @@ func GetInstance() *CallInfo {
 			PORT: "8090",
 			PATH: path{
 				GET: get{
-					METHOD:     "GET",
-					BookSearch: "/book/list",
+					METHOD:   "GET",
+					BookList: "/book/list",
 				},
 				POST: post{
 					METHOD:     "POST",
 					BookRegist: "/book/registration",
+				},
+			},
+		}
+	}
+	return instance
+}
+
+func INPUTInstance(INPUT string) *CallInfo {
+	if instance == nil {
+		instance = &CallInfo{
+			HOST: "http://localhost",
+			PORT: "8090",
+			PATH: path{
+				GET: get{
+					METHOD:             "GET",
+					BookTitleSearch:    "/book/search/" + INPUT,
+					BookDetailSearch:   "/book/search/" + INPUT + "/detail",
+					RentUserInfoSearch: "/book/search/" + INPUT + "/user",
+					BookSearch_Update:  "/book/search/" + INPUT + "/change",
+				},
+				POST: post{
+					METHOD:             "POST",
+					BookRegistDetail:   "/book/registration/" + INPUT + "/detail",
+					RentUserInfoRegist: "/book/rent/" + INPUT + "/user",
+				},
+				PUT: put{
+					METHOD:     "PUT",
+					BookUpdate: "/book/search/" + INPUT + "/change",
+					BookRent:   "/book/rent/" + INPUT,
+					BookReturn: "/book/return/" + INPUT,
+				},
+				DELETE: delete{
+					METHOD:               "DELETE",
+					BookRemove:           "/book/search/" + INPUT + "/del",
+					ReturnUserInfoRemove: "/book/return/" + INPUT + "/user",
 				},
 			},
 		}
