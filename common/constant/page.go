7c5d865b9ad251368488dea9b89f6cf3422ca8be
WGCAT <seed9878@naver.com>
2024-02-28 21:44:23 +0900

    Feat : page수정

--- common/constant/page.go
+++ common/constant/page.go
@@ -2,35 +2,69 @@ package constant
 
 // 메인 메뉴
 const (
-	MainPage       = 1000
-	BookManagePage = 1100
-	BookRentPage   = 1200
-	BookReturnPage = 1300
+	MainPage       string = "1000"
+	BookManagePage string = "1100"
+	BookRentPage   string = "1200"
+	BookReturnPage string = "1300"
 )
 
 // 책 관리
 const (
-	BookManageRegistPage = 1110
-	BookManageSearchPage = 1120
+	BookManageRegistPage string = "1110"
+	BookManageSearchPage string = "1120"
+	BookManageUpdatePage string = "1130"
+	BookManageRemovePage string = "1140"
 )
 
-// 대출
+//책 등록
 const (
-	BookRentRegistPage = 1210
+	BookManageInfoRegistPage   string = "1111"
+	BookManageDetailRegistPage string = "1112"
 )
 
-// 반납
+// 책 검색
 const (
-	BookReturnRegistPage = 1310
+	BookManageAllSearchPage          string = "1121"
+	BookManageTitleSearchPage        string = "1122"
+	BookManageDetailSearchPage       string = "1123"
+	BookManageRentUserInfoSearchPage string = "1124"
 )
+
+// 메뉴 마지막
+const (
+	LastPage = 9999
+)
+
+// // 대출
+// const (
+// 	BookRentRegistPage = 1210
+// )
+
+// // 반납
+// const (
+// 	BookReturnRegistPage = 1310
+// )
 
 // 사용자 오퍼레이션
 const (
-	OperationMain   string = "1"
-	OperationBook   string = "2"
-	OperationRent   string = "3"
-	OperationExit   string = "e"
-	OperationBack   string = "b"
-	OperationMenu   string = "m"
+	OperationBook   string = "1"
+	OperationRent   string = "2"
+	OperationReturn string = "3"
+
+	OperationBack string = "b"
+	OperationMenu string = "m"
+	OperationExit string = "e"
+
 	OperationRegist string = "1"
+	OperationSearch string = "2"
+	OperationUpdate string = "3"
+	OperationRemove string = "4"
+
+	OperationInfoRegist   string = "1"
+	OperationDetailRegist string = "2"
+
+	OperationAllSearch      string = "1"
+	OperationTitleSearch    string = "2"
+	OperationDetailSearch   string = "3"
+	OperationUserInfoSearch string = "4"
 )
