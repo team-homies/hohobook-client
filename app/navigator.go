2997e617b432360367ca8c9b1125a2d22cbfefe0
SH2G <suhy427@gmail.com>
2024-03-03 21:34:04 +0900

    Chore : 뼈대

--- app/navigator.go
+++ app/navigator.go
@@ -18,23 +18,24 @@ func Navigate(operation int) (page []string, err error) {
 			page = append(page, "[b.뒤로가기, m.메인메뉴, e.종료]")
 			page = append(page, "원하는 키워드를 입력하고 Enter를 누르세요.")
 		}
-	case constant.BookRentPage:
-		{
-			page = append(page, "메인 -> 대출 메뉴입니다.")
-			page = append(page, "[1.대출 등록, 2.대출 조회]")
-			page = append(page, "[b.뒤로가기, m.메인메뉴, e.종료]")
-			page = append(page, "원하는 키워드를 입력하고 Enter를 누르세요.")
-		}
+
 	case constant.BookManageRegistPage:
 		{
-			page = append(page, "책 등록")
+			page = append(page, "책 등록 메뉴입니다")
 			page = append(page, "[1.책 재등록, b.뒤로가기, m.메인메뉴, e.종료]")
 			page = append(page, "원하는 키워드를 입력하고 Enter를 누르세요.")
 		}
 	case constant.BookManageSearchPage:
 		{
-			page = append(page, "책 조회")
+			page = append(page, "책 조회 메뉴입니다.")
 			page = append(page, "[1.책 전체 조회, 2.책 제목 조회, 3.책 상세 조회, 4.대출자 조회]")
+			page = append(page, "[b.뒤로가기, m.메인메뉴, e.종료]")
+			page = append(page, "원하는 키워드를 입력하고 Enter를 누르세요.")
+		}
+	case constant.BookRentPage:
+		{
+			page = append(page, "메인 -> 대출 메뉴입니다.")
+			page = append(page, "[1.대출 등록, 2.대출 조회]")
 			page = append(page, "[b.뒤로가기, m.메인메뉴, e.종료]")
 			page = append(page, "원하는 키워드를 입력하고 Enter를 누르세요.")
 		}
--- ui.go
+++ ui.go
@@ -30,35 +30,3 @@ func printBookSearch() {
 원하는 키워드를 입력하고 Enter를 누르세요.
 `)
 }
-
-// case "3": //수정
-// 			var title string
-// 			var putChoice string
-// 			var column string
-// 			var str string
-// 			var value string
-
-// 			fmt.Println()
-// 			fmt.Print("책 제목 입력> ") //책 제목 입력창
-// 			fmt.Scanln(&title)
-
-// 			for {
-// 				PutBookMenu() //수정목록 ui
-// 				fmt.Print("입력: ")
-// 				fmt.Scanln(&putChoice) //수정할 번호 선택
-// 				switch putChoice { //컬럼을 반환한다.
-// 				case "1": column = "title"; str = "제목"
-// 				case "2": column = "author"; str = "저자"
-// 				case "3": column = "publisher"; str = "출판사"
-// 				case "4": column = "theme"; str = "장르"
-// 				case "5": column = "isbn"; str = "ISBN"
-// 				case "b", "m", "e":
-// 					Common(putChoice)
-// 				default:
-// 					fmt.Println("잘못된 입력입니다. 다시 시도하세요.")
-// 					main()
-// 				}
-// 				fmt.Print(str, " 입력> ") //입력값을 반환한다.
-// 				fmt.Scanln(&value)
-// 				PutBook(title, column, value) //값 받아와서 JSON형식으로 서버에 넘기는 함수
-// 			}
